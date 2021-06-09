44d4aa61545af8ceda13073815a1d6aaf840800c
Kaiser Samrat <kaisersamrat136@gmail.com>
2021-06-09 18:41:12 +0600

    navbar added

---
+++ src/components/Main/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div>
+            <div className="bg-white h-screen flex flex-col justify-center items-center">
+
+            </div>
+        </div>
+    );
+};
+
+export default Content;
