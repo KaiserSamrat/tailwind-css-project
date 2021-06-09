5afe293c6c8582957c0576e41bea0cef371b7bd1
Kaiser Samrat <kaisersamrat136@gmail.com>
2021-06-09 19:02:45 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Header from './components/Header/Header';
 function App() {
   return (
     <div >
      <Router>
      <Navbar/>
+     <Header/>
      </Router>
     </div>
   );
