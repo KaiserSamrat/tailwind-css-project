71716c0c56185a0fdabd0e3851f81191c3aa4500
Kaiser Samrat <kaisersamrat136@gmail.com>
2021-06-09 23:37:15 +0600

    items added

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,18 +6,18 @@ const Item = () => {
     return (
         <>
             <div className='menu-card'>
-                <img src={Pizza} alt='egg' className='h-full rounded mb-20 shadow' />
+                <img src={Pizza} alt='egg' className='h-full rounded mb-10 shadow' />
                 <div className='center-content'>
-                    <h2 className='text-2xl mb-2'>Egg Muffins</h2>
-                    <p className='mb-2'>Cripsy, delicious, and nutritious</p>
+                    <h2 className='text-2xl mb-2'>Pizza</h2>
+                    <p className='mb-2'>Chicken, 8 Inch, and nutritious</p>
                     <span>$16</span>
                 </div>
             </div>
             <div className='menu-card'>
-                <img src={Burger} alt='egg' className='h-full rounded mb-20 shadow' />
+                <img src={Burger} alt='egg' className='h-full rounded mb-10 shadow' />
                 <div className='center-content'>
-                    <h2 className='text-2xl mb-2'>Egg Salad</h2>
-                    <p className='mb-2'>Cripsy, delicious, and nutritious</p>
+                    <h2 className='text-2xl mb-2'>Burger</h2>
+                    <p className='mb-2'>Chicken, Double Petty, and nutritious</p>
                     <span>$18</span>
                 </div>
             </div>
